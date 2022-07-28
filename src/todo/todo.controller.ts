614441be2af9d2078e35d1935181a9e8aa137907
FIRST_NAME LAST_NAME <louguefranck.20@gmail.com>
2022-07-28 23:36:38 +0000

    create crud todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,55 +1,58 @@
-import { Body, Controller, Delete, Get, Post, Put, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
+import { Todo } from './entities/todo.entitie';
 
 @Controller('todo')
 export class TodoController {
-@Get("v2")
-getTodoV2(
-    @Req() request: Request,
-    @Res() response: Response
-){
-    console.log("Récupérer la liste des todos");
+  constructor() {
+    this.todos = [];
+  }
+  todos: Todo[];
+  @Get('v2')
+  getTodoV2(@Req() request: Request, @Res() response: Response) {
+    console.log('Récupérer la liste des todos');
     response.status(202);
-  response.json({
-        contenu: `Je suis une response générée à partie de l'objet response`
-    })
+    response.json({
+      contenu: `Je suis une response générée à partie de l'objet response`,
+    });
+  }
 
+  @Get()
+  getTodo() {
+    console.log('Récupérer la liste des todos');
+
+    return this.todos;
+  }
+
+  @Post()
+  addTodo(@Body() newTodo: Todo) {
+    if (this.todos.length) {
+      newTodo.id = this.todos[this.todos.length - 1].id + 1;
+    } else {
+      newTodo.id = 1;
+    }
+    this.todos.push(newTodo);
+    return newTodo;
+  }
+
+  @Delete()
+  deleteTodo() {
+    console.log('Supprimer un todo de la liste des todos');
+    return 'Delete Todo';
+  }
+
+  @Put()
+  modifierTodo() {
+    console.log("Modifier l'un des todo de la liste des todos");
+    return 'Update Todo';
+  }
 }
-
-@Get()
-getTodo(
-   
-){
-    console.log("Récupérer la liste des todos");
-   
-   return "Listes des todos"
-
-}
-
-@Post()
-addTodo(
-    @Body('id') id: string,
-    @Body('name') name: string,
-    @Body('description') description: string
-){
-
-    console.log("Ajouter un todo a la liste des todos");
-    console.log(id, name, description)
-    return "Add Todo"
-}
-
-@Delete()
-deleteTodo(){
-    console.log("Supprimer un todo de la liste des todos");
-    return "Delete Todo"
-}
-
-
-@Put()
-modifierTodo(){
-    console.log("Modifier l'un des todo de la liste des todos")
-    return "Update Todo"
-}
-
-}
-
