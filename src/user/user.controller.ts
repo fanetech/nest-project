c206a9213de21356dc3aaa97ceb890cd04096402
haroun95a <ouedraogoh07@gmail.com>
2022-08-08 01:38:13 +0000

    coding user login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,14 +2,25 @@ import { UserService } from './user.service';
 import { UserEntity } from './entites/user.entity';
 import { UserSubscriberDto } from './dto/User-subscriber.dto';
 import { Body, Controller, Post } from '@nestjs/common';
+import { LoginCredentialDto } from './dto/login-credentials.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
-  @Post()
+
+
+  @Post("register")
   async register(
     @Body() userData: UserSubscriberDto,
   ): Promise<Partial<UserEntity>> {
     return this.userService.register(userData);
+  }
+
+
+  @Post("login")
+  async login(
+    @Body() credential: LoginCredentialDto
+  ): Promise<Partial<UserEntity>>{
+    return this.userService.login(credential)
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,23 @@
 import { UserEntity } from './entites/user.entity';
 import { Repository } from 'typeorm';
 import { UserSubscriberDto } from './dto/User-subscriber.dto';
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
+import { LoginCredentialDto } from './dto/login-credentials.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
-  ) {}
+  ) { }
+
+  
   async register(userData: UserSubscriberDto): Promise<Partial<UserEntity>> {
     const user = this.userRepository.create({ ...userData });
     user.salt = await bcrypt.genSalt();
@@ -23,5 +30,29 @@ export class UserService {
     delete user.password;
     delete user.salt;
     return user;
+  }
+
+  async login(credential: LoginCredentialDto): Promise<Partial<UserEntity>> {
+    const { username, password } = credential;
+    const user = this.userRepository
+      .createQueryBuilder('user')
+      .where('user.username= :username or user.email = :username', {
+        username,
+      })
+      .getOne();
+    if (!user) {
+      throw new NotFoundException('identifiant inalide');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, (await user).salt);
+    if (hashedPassword === (await user).password) {
+      return {
+        username,
+        email: (await user).email,
+        role: (await user).role,
+      }
+    } else {
+      throw new NotFoundException('identifiant inalide');
+    }
   }
 }
